2a265ae6360fafaebcbdc9f6f312d58fd721a1d6
deepredk <deepred.kim@gmail.com>
2022-05-11 19:15:16 +0900

    Spring Web + MariaDB + JPA(이하 MariaDB웹) 어플리케이션 기본 골격

---
+++ mariadb-api-sample/src/main/java/learn/msa/mariadbapisample/MariadbApiSampleApplication.java
@@ -0,0 +1,13 @@
+package learn.msa.mariadbapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariadbApiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariadbApiSampleApplication.class, args);
+	}
+
+}
---
+++ mariadb-api-sample/src/test/java/learn/msa/mariadbapisample/MariadbApiSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.msa.mariadbapisample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariadbApiSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
